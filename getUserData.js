55b6374331de12052ce8ce2dda9db7379914a6d9
81GB3N <jonas.balukonis@gmail.com>
2024-01-02 14:41:37 +0200

    transfered getUserData and checkUserStatus functions

--- getUserData.js
+++ getUserData.js
@@ -1,4 +1,4 @@
-export default async function fetchUserData(userData, page) {
+export async function sendUserData(userData, page) {
     await fetch(`http://localhost:3000/${page}`, {
       method: "POST",
       headers: {
@@ -9,4 +9,16 @@ export default async function fetchUserData(userData, page) {
       .then((response) => response.json())
       .then((data) => console.log(data))
       .catch((error) => console.error("Error:", error));
-  }
+}
+
+export async function getUserData(name, surname){
+  const response = await fetch(`http://localhost:3000/api?name=${name}&surname=${surname}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function checkUserStatus(name, surname){
+  const userDagetUserData = await getUserData(name, surname);
+  const usersAmount = Object.keys(userDagetUserData.result).length;
+  return usersAmount > 0 ? true : false;
+}
