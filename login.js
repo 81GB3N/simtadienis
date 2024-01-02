7a798aae231c596244560c660278e3b037d933af
81GB3N <jonas.balukonis@gmail.com>
2024-01-02 14:42:37 +0200

    added full validation

--- login.js
+++ login.js
@@ -1,8 +1,26 @@
-import fetchUserData from "./getUserData.js";
+import {getUserData, checkUserStatus} from "./getUserData.js";
 
 const loginForm = document.querySelector(".login-form");
 
-loginForm.addEventListener("submit", (e) => {
+async function UserInfo(name, surname, password){
+  const info = await getUserData(name, surname);
+  // console.log(info.result[0], password === info.password ? true : false, info.result.password)
+  return password === info.result[0].password ? true : false;
+}
+
+function userDoesntExist(){
+  console.log("user doesnt exist");
+}
+
+function badLoginInfo(){
+  console.log("the entered user info doesnt match the password");
+}
+
+function loadingProfile(){
+  console.log("loading your profile");
+}
+
+loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const name = document.querySelector("#name").value;
@@ -13,8 +31,15 @@ loginForm.addEventListener("submit", (e) => {
     name: name,
     surname: surname,
     password: password,
-};
-//
-
-  fetchUserData(userData, "login");
+  };
+if(await checkUserStatus(userData.name, userData.surname) === false){
+  userDoesntExist();
+  return;
+}
+else if(await UserInfo(userData.name, userData.surname, userData.password) === false){
+  badLoginInfo();
+  return;
+}
+loadingProfile();
+  // fetchUserData(userData, "login");
 });
