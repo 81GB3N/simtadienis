48cefa628a30d97f014d0ee6ee7c4767deaa08b0
81GB3N <jonas.balukonis@gmail.com>
2024-01-04 01:10:36 +0200

    exportable updateLeaderbpard function

--- leaderboard.js
+++ leaderboard.js
@@ -2,26 +2,26 @@ import { getAllUsers } from "./getUserData.js";
 
 const leaderboard = document.querySelector(".leaderboard");
 
-async function all(){
-    const allUsers = await getAllUsers();
-    // console.log(allUsers.result);
-    return allUsers.result;
+async function all() {
+  const allUsers = await getAllUsers();
+  return allUsers.result;
 }
 
-// const allUsers = all();
+
+//TODO: make updateLeaderboard() function work exported when admin enters new value with react
+// export default async function updateLeaderboard() {
 const allUsers = await all();
-allUsers.sort((a,b)=>b.money-a.money);
-console.log(allUsers);
-leaderboard.innerHTML = ``;
+allUsers.sort((a, b) => b.money - a.money);
+
 let cnt = 0;
-for(let x in allUsers){
-    if(!allUsers[x].admin){
-        let listElement = `<li>Place: ${Number(cnt)+1} Name/Surname: ${allUsers[x].name} ${allUsers[x].surname} Networth: ${allUsers[x].money}</li>`;
-        leaderboard.innerHTML += listElement;
-        cnt++;
+leaderboard.innerHTML = ``;
+  for (let x in allUsers) {
+    if (!allUsers[x].admin) {
+      let listElement = `<li>Place: ${Number(cnt) + 1} Name/Surname: ${allUsers[x].name} ${allUsers[x].surname} Networth: ${allUsers[x].money}</li>`;
+      leaderboard.innerHTML += listElement;
+      cnt++;
     }
-}
-// console.log(allUsers)
+  }
+// }
 
-// allUsers.sort((a,b)=>a.money - b.money);
-
+// updateLeaderboard();
