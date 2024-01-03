1f5c79caedb44007c873ca9776567e53f6abd8a7
81GB3N <jonas.balukonis@gmail.com>
2024-01-03 15:20:15 +0200

    added user money display

--- user.js
+++ user.js
@@ -1,3 +1,5 @@
+import { getUserData } from "./getUserData.js";
+
 const user = document.querySelector(".username");
 const moneyAmount = document.querySelector(".money-amount");
 const logoutButton = document.querySelector(".logout");
@@ -11,8 +13,17 @@ logoutButton.addEventListener('click', logout);
 
 // console.log(localStorage.getItem("user") !== null);
 
-if(localStorage.getItem("user") !== null){
-    const userData = JSON.parse(localStorage.getItem("user"));
-    user.innerHTML = `${userData.name} ${userData.surname}`;
+if(localStorage.getItem("user") === null) logout()
+
+const userData = JSON.parse(localStorage.getItem("user"));
+user.innerHTML = `${userData.name} ${userData.surname}`;
+
+async function getMoneyAmount(){
+const allData = await getUserData(userData.name, userData.surname);
+const data = await allData;
+return data.result[0].money;
 }
-else logout();
+
+const userMoney = await getMoneyAmount();
+
+moneyAmount.innerHTML = userMoney;
