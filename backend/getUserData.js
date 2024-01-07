c3eea8f5ea41fb7ccba395bd235e0532618c436f
81GB3N <jonas.balukonis@gmail.com>
2024-01-07 23:26:44 +0200

    changed user data functions to be accesible and added baseUrl

--- backend/getUserData.js
+++ backend/getUserData.js
@@ -9,9 +9,10 @@ const port = 4000;
 //     body: JSON.stringify(port),
 //   })
 // }
+const baseUrl = window.location.hostname === 'localhost' ? `http://localhost:${port}` : '';
 
 export async function sendUserData(userData, page) {
-  await fetch(`http://localhost:${port}/api/${page}`, {
+  await fetch(`${baseUrl}/api/${page}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -22,14 +23,14 @@ export async function sendUserData(userData, page) {
 
 export async function getUserData(name, surname) {
   const response = await fetch(
-    `http://localhost:${port}/api/getuser?name=${name}&surname=${surname}`
+    `${baseUrl}/api/getuser?name=${name}&surname=${surname}`
   );
   const data = await response.json();
   return data;
 }
 
 export async function getAllUsers(){
-  const response = await fetch(`http://localhost:${port}/api/getallusers`)
+  const response = await fetch(`${baseUrl}/api/getallusers`)
   const data = await response.json();
   return data;
 }
