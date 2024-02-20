e3eabaebbec666ae7ae7fcd916b221a1f4513b41
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2024-02-20 17:18:16 +0200

    Change to mobile error handling

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -6,20 +6,33 @@ import LeaderBoard from "../components/LeaderBoard"
 import ErrorModal from "../components/ErrorModal"
 // Context provider
 import MenuProvider from '../context/MenuProvider';
+import { useEffect, useState } from "react";
 
 export default function LandingPage() {
+
+    const [showModal, setShowModal] = useState(false);
+
     const isMobile = () => {
         const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
         return regex.test(navigator.userAgent);
     };
 
+    useEffect(() => {
+        setShowModal(!isMobile());
+    }, [])
+
+    if (showModal) {
+        return (<ErrorModal
+            status='Mobile Device Required!'
+            errorMessage='This website was designed for mobile devices. 
+      Please use a mobile device to proceed'
+            dismissable={true}
+            dismiss={() => setShowModal(false)}
+        />)
+    }
+
     return (
         <MenuProvider>
-            {!isMobile() ? <ErrorModal
-                status='Desktop Detected!'
-                errorMessage='This website was designed for mobile devices. 
-      Proceed at your discretion'
-                dismissable={true} /> : null}
             <Header />
             <div className="landing">
                 <HeroTicket />
