71a9edc24e80e12fa12356b9da36736373041054
81GB3N <jonas.balukonis@gmail.com>
2024-02-23 16:41:43 +0200

    backarrow integrated

--- frontend/src/components/user/NoUser.jsx
+++ frontend/src/components/user/NoUser.jsx
@@ -2,10 +2,14 @@ import { useState } from "react"
 import { FormattedMessage } from 'react-intl';
 import Login from "./Login";
 import SignUp from "./SignUp";
+import { useMenu } from '../../context/MenuProvider';
+
 import UserGreeting from "./UserGreeting";
+import BackArrow from "../backArrow";
 export default function NoUser({ setUserExists, firstTime, endGreeting }) {
     const [login, setLogin] = useState(false);
     const [signup, setSignup] = useState(false);
+    const { toggleMenu } = useMenu();
 
     if (login) {
         return <Login
@@ -25,9 +29,12 @@ export default function NoUser({ setUserExists, firstTime, endGreeting }) {
     //         <UserGreeting endGreeting={endGreeting}/>
     //     )
     // }
-
+    
     return (
         <div className="user__buttons">
+             <button className="form-leave" onClick={toggleMenu}>
+             <BackArrow/>
+            </button>
             <button className="user-button login-btn" onClick={() => setLogin(!login)}>
                 <FormattedMessage id='login' />
             </button>
