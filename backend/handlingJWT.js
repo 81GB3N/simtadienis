86d20eb9b21302f78c520edd7128a30efb2d25cf
81GB3N <jonas.balukonis@gmail.com>
2024-03-01 22:05:33 +0200

    JWT handling function

---
+++ backend/handlingJWT.js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { getUserToken } = require('./db');
+
+const secretKey = process.env.JWT_SECRET_KEY;
+// console.log(secretKey)
+
+const generateJWT = (user, role) => {
+
+    const payload = {
+        name: user.name,
+        surname: user.surname,
+        role: role,
+    };
+        
+    // Generate JWT token
+    const token = jwt.sign(payload, secretKey); 
+    
+    console.log("generated token:", token);
+    return token;
+}
+
+// generateJWT({name: "super", surname: "admin"}, "super admin");
+
+function verifyToken(req, res, next) {
+
+    // Extract the JWT token from the Authorization header
+    const token = req.headers.authorization.startsWith('Bearer') ? req.headers.authorization.split(' ')[1] : req.headers.authorization;
+    console.log("authorization bearer handlingJWT verifyToken: ", token);
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: No token provided' });
+    }
+
+    // Verify the token
+    jwt.verify(token, secretKey, (err, decoded) => {
+        console.log("token:", token, "secretKey: ", secretKey)
+        if (err) {
+            console.log(err);
+            return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+        }
+        
+        req.payload = decoded;
+        console.log("PAYLOAD:", req.payload)
+        next(); // Proceed to the next middleware or route handler
+    });
+}
+
+async function getJWT(name, surname){
+    const token = await getUserToken(name, surname);
+    console.log("token from db: ", token);
+    return token;
+}
+
+module.exports = { generateJWT, verifyToken, getJWT }
