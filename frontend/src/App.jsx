7728258ef7c28e6e9cba19cecff1c09bf2437c88
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2024-03-11 23:37:34 +0200

    Removed formPage from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import './css/app.css';
 
-import { Routes, Route, Form } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import UserPage from './pages/UserPage';
 import LeaderBoardPage from './pages/LeaderBoardPage';
 import AdminPage from './pages/AdminPage';
 import SuperAdminPage from './pages/SuperAdminPage';
-import FormPage from './pages/FormPage';
 import ErrorModal from './components/modal/ErrorModal';
 
 import PageProvider from './context/PageProvider';
@@ -14,16 +13,9 @@ import PageProvider from './context/PageProvider';
 /**
  * The main component of the application.
  * Renders different pages based on the current route.
- * @param {boolean} formMode - Whether the application is being rendered to only display the event google form.
  * @returns {JSX.Element} The rendered application component.
  */
-export default function App({ formMode }) {
-
-  if (formMode) {
-    return (
-      <FormPage />
-    )
-  }
+export default function App() {
 
   return (
     <Routes>
