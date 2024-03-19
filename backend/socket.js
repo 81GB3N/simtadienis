c24531177c91cbe18c02f8d5e93b70e63d8f34c2
81GB3N <jonas.balukonis@gmail.com>
2024-03-19 12:05:50 +0200

    working socket code

--- backend/socket.js
+++ backend/socket.js
@@ -1,9 +1,13 @@
 const { Server } = require('socket.io');
 
 function initializeSocket(server){
+
+  const isLocalhost = window.location.hostname === 'localhost';
+  const origin = isLocalhost ? 'http://localhost:3000' : 'https://lic100.lt';  
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your React frontend URL
+    origin: origin, // Replace with your React frontend URL
     methods: ["GET", "POST"],
   },
 });
