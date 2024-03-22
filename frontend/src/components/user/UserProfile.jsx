aa16175434e4f55b61164e255de6e92b5a463311
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2024-03-22 02:59:42 +0200

    voteId dependency

--- frontend/src/components/user/UserProfile.jsx
+++ frontend/src/components/user/UserProfile.jsx
@@ -58,7 +58,7 @@ export default function UserProfile() {
                 changeVoteId(index, data.votes[index]);
             });
         });
-    }, [fetchData, changeVoteId])
+    }, [fetchData, changeVoteId, userId.name, userId.surname])
 
     /**
      * Opens the edit profile modal.
